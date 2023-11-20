e0606ac4b35725ee6664e713075ee0188a8c86b4
MiguelMata <92826930+MiguelMata1964@users.noreply.github.com>
2023-11-20 15:51:46 -0600

    Add files via upload

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: '.',
+	globPatterns: [
+		'**/*.{png,jpg,html,json,mp3,txt,svg}'
+	],
+	swDest: 'D:/AppServ/www/PatoNadando/sw.js',
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	]
+};
